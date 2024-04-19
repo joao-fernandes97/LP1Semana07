4ff9384ea4eae764d6518b6d3cf801b2faa58564
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-04-19 16:03:35 +0100

    Unit modified

--- GameUnits/Unit.cs
+++ GameUnits/Unit.cs
@@ -1,14 +1,25 @@
 namespace GameUnits
 {
-    public class Unit
+    //class has an abstract property therefore it
+    //must be abstract as well
+    public abstract class Unit
     {
         private int movement;
-        public int Health { get; set; }
+        //property must be virtual because we intend
+        //to override it when implementing a class
+        //that extends this one
+        public virtual int Health { get; set; }
+        public abstract float Cost { get; }
 
         public Unit(int movement, int health)
         {
             this.movement = movement;
             Health = health;
+        }
+
+        public void Move()
+        {
+            Console.WriteLine(movement);
         }
     }
 }
